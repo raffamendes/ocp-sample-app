3538257578508c06580abe0ef1b5599bbe96ff60
Raffael Mendes <rmendes@redhat.com>
2025-03-27 14:28:19 -0300

    adding podname endpoint

--- src/main/java/com/rmendes/SampleResource.java
+++ src/main/java/com/rmendes/SampleResource.java
@@ -1,16 +1,22 @@
 package com.rmendes;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class GreetingResource {
+@Path("/ocp")
+public class SampleResource {
+	
+	@ConfigProperty(name = "pod.name")
+	String podName;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    @Path("/podName")
+    public String getPodName() {
         return "Hello from Quarkus REST";
     }
 }
