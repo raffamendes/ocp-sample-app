fdfe7d9f6f2f77bf6601e74ce582882f0816bfa9
Raffael Mendes <rmendes@redhat.com>
2025-06-05 16:23:16 -0300

    adding opentelemetry and micrometer capabilities

--- src/main/java/com/rmendes/SampleResource.java
+++ src/main/java/com/rmendes/SampleResource.java
@@ -2,6 +2,7 @@ package com.rmendes;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import io.micrometer.core.annotation.Timed;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -12,10 +13,12 @@ public class SampleResource {
 	
 	@ConfigProperty(name = "pod.name")
 	String podName;
-
+	
+	
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/podName")
+    @Timed(value="pod_name_latency")
     public String getPodName() {
         return "PodName: "+podName;
     }
